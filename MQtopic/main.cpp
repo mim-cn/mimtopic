4e017950813dda7b5572dadc9db1f3bcffdbb5d9
skybosi <skybosi_fxc@163.com>
2017-08-09 00:42:38 +0800

    c++ Encapsulation

---
+++ MQtopic/main.cpp
@@ -0,0 +1,33 @@
+#include "rbtrees.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define NUM_NODES 32
+
+int main()
+{
+    rbtrees rb;
+	/* *insert */
+	int i = 0;
+	printf("insert node from 1 to NUM_NODES(32): \n");
+	for (; i < NUM_NODES; i++) {
+        rb.add(&i,sizeof(i));
+	}
+    rb.traverse();
+    /* *delete */
+    printf("delete node 20: \n");
+    int d = 20;
+    rb.erase(&d);
+    printf("delete node 10: \n");
+    d = 10;
+    rb.erase(&d);
+    printf("delete node 15: \n");
+    d = 15;
+    rb.erase(&d);
+    rb.traverse();
+	return 0;
+}
+
+
